9dbba0a5770886927cf7dd7543a4a9fe3fc0bde9
Nagargoje Abhishek <nagargojeabhishek96@gmail.com>
2026-05-13 10:24:30 +0530

    Prepare Vercel SPA routing

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/CA_Vikas_Official/',
+  base: '/',
 
   plugins: [
     react(),
